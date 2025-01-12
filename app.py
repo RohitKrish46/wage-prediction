7b19ebb383f4e68bda4536b8eab67fa6d2636660
Rohit Krishnan <krishr.somu@gmail.com>
2025-01-12 18:08:35 +0530

    made changes to app.py

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import pickle
 
 app = Flask(__name__)
 
-with open('/models/model.pkl', 'rb') as file:
+with open('./models/model.pkl', 'rb') as file:
     model=pickle.load(file)
 
-with open('/models/preprocessor.pkl', 'rb') as file:
+with open('./models/preprocessor.pkl', 'rb') as file:
     model=pickle.load(file)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('test.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
